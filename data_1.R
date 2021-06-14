77d3d5ebad01f2ec08cd308d76505ef4f0f2e54e
Taubitz <taubitzj@charite.de>
2021-06-14 17:18:24 +0200

    new visuals

--- data_1.R
+++ data_1.R
@@ -1,11 +1,10 @@
 ########################### Load in libraries ###########################
 
-library(tidyverse)
-library(ggplot2)
+
 library(highcharter)
 library(janitor)
-library(readr)
 library(readxl)
+library(tidyverse)
 library(writexl)
 
 
@@ -18,11 +17,17 @@ raw_data <- read_excel("Endfassung.xlsx",
 #                         sheet = "Worksheet")
 
 ########################### Clean data ###########################
+
+oa_status_colors <- c("gold", "hybrid", "green", "bronze", "closed", "kein ergebnis")
+color <- c("#F4C244", "#A0CBDA", "#4FAC5B", "#D85DBF", "#2C405E", "#5F7036")
+#color <- c("#5cff0a", "#ffdc0a", "#0ac7ff", "#cd7f32", "#262c2f", "darkgray")
+
+
 data <- raw_data %>%
   clean_names() %>%
   mutate(oa_status = tolower(oa_status)) %>%
   mutate(oa_status = replace_na(oa_status, "kein ergebnis")) %>%
-  mutate(oa_status = factor(oa_status, levels = c("green", "gold", "hybrid", "bronze", "closed", "kein ergebnis"))) %>%
+  mutate(oa_status = factor(oa_status, levels = oa_status_colors)) %>%
   mutate(corresponding_author_cha = if_else(row_number() %in% c(1:6292), TRUE, FALSE), .after = "oa_status")
 
 
@@ -45,7 +50,7 @@ data_sum <- data %>%
   summarise(value = n()) %>%
   mutate(percent = round(value / sum(value) * 100, 1))
 
-color <- c("#5cff0a", "#ffdc0a", "#0ac7ff", "#cd7f32", "#262c2f", "darkgray")
+
 text <- "In hac habitasse platea dictumst. Nam hendrerit elementum lacus. Suspendisse potenti. Vestibulum id aliquet neque. Praesent vel est est. Integer molestie consequat erat nec facilisis. Pellentesque scelerisque posuere nulla eu interdum."
 
 status_absolute <-
@@ -53,7 +58,8 @@ status_absolute <-
          "column",
          hcaes(x = jahr, y = value, group = oa_status)) %>%
   hc_plotOptions(series = list(stacking = "normal")) %>%
-  hc_colors(color)
+  hc_colors(color) %>%
+  hc_yAxis(reversedStacks = FALSE)
 
 # %>% hc_title(text = "Open access status in absolute numbers", align = "left", style = list(fontSize = "12px"))
 # %>% hc_subtitle(text = text, align = "left", style = list(fontSize = "12px"))
@@ -65,7 +71,9 @@ status_percent <-
   hc_plotOptions(series = list(stacking = "normal")) %>%
   hc_colors(color) %>%
   hc_yAxis(labels = list(format = '{value} %'),
-           max = 100)
+           max = 100, reversedStacks = FALSE)
+
+# reversed bar stacks https://www.highcharts.com/forum/viewtopic.php?t=10916
 
 # hc_title(text = "Open access status of articles in %", align = "left", style = list(fontSize = "12px")) %>%
 # hc_subtitle(text = text, align = "left", style = list(fontSize = "12px")) %>%
@@ -80,7 +88,6 @@ data_corresponding_sum <- data %>%
   summarise(value = n()) %>%
   mutate(percent = round(value / sum(value) * 100, 1))
 
-color <- c("#5cff0a", "#ffdc0a", "#0ac7ff", "#cd7f32", "#262c2f", "darkgray")
 text <- "In hac habitasse platea dictumst. Nam hendrerit elementum lacus. Suspendisse potenti. Vestibulum id aliquet neque. Praesent vel est est. Integer molestie consequat erat nec facilisis. Pellentesque scelerisque posuere nulla eu interdum."
 
 status_corresponding_absolute <-
@@ -90,7 +97,8 @@ status_corresponding_absolute <-
     hcaes(x = jahr, y = value, group = oa_status)
   ) %>%
   hc_plotOptions(series = list(stacking = "normal")) %>%
-  hc_colors(color)
+  hc_colors(color) %>%
+  hc_yAxis(reversedStacks = FALSE)
 
 
 # hc_title(text = "Open access status of articles of which a Charité scientists was the corresponding author(in absolute numbers)", align = "left", style = list(fontSize = "28px")) %>%
@@ -105,10 +113,54 @@ status_corresponding_percent <-
   hc_plotOptions(series = list(stacking = "normal")) %>%
   hc_colors(color) %>%
   hc_yAxis(labels = list(format = '{value} %'),
-           max = 100)
+           max = 100, reversedStacks = FALSE)
 
 # hc_title(text = "Open access status of articles of which a Charité scientists was the corresponding author (in %)", align = "left", style = list(fontSize = "28px")) %>%
 # hc_subtitle(text = text, align = "left", style = list(fontSize = "12px")) %>%
+
+
+item_2018 <- data_corresponding_sum %>%
+  filter(jahr == 2018) %>%
+  hchart(
+    "item",
+    hcaes(name = oa_status, y = value),
+    name = "oa_status",
+    marker = list(symbol = "square"),
+    showInLegend = TRUE
+  ) %>%
+  hc_title(
+    text = "2018"
+  ) %>%
+  hc_colors(color)
+
+item_2019 <- data_corresponding_sum %>%
+  filter(jahr == 2019) %>%
+  hchart(
+    "item",
+    hcaes(name = oa_status, y = value),
+    name = "oa_status",
+    marker = list(symbol = "square"),
+    showInLegend = TRUE
+  ) %>%
+  hc_title(
+    text = "2019"
+  ) %>%
+  hc_colors(color)
+
+item_2020 <- data_corresponding_sum %>%
+  filter(jahr == 2020) %>%
+  hchart(
+  "item",
+  hcaes(name = oa_status, y = value),
+  name = "oa_status",
+  marker = list(symbol = "square"),
+  showInLegend = TRUE
+) %>%
+  hc_title(
+    text = "2020"
+  ) %>%
+  hc_colors(color)
+
 
 ########################### Journals ###########################
 
@@ -125,7 +177,7 @@ journal_data_2 <- journal_data %>%
   group_by(zeitschrift) %>%
   spread(oa_status, value, fill = 0) %>%
   gather(oa_status, value, 3:8) %>%
-  mutate(oa_status = factor(oa_status, levels = c("green", "gold", "hybrid", "bronze", "closed", "kein ergebnis"))) %>%
+  mutate(oa_status = factor(oa_status, levels = oa_status_colors)) %>%
   mutate(percent = round(value / sum(value) * 100, 1))
 
 ########################### Plot journals ###########################
@@ -137,7 +189,8 @@ journal_absolute <- journal_data_2 %>%
   hc_xAxis(min = 0,
            max = 20,
            scrollbar = list(enabled = TRUE)) %>%
-  hc_size(height = 500)
+  hc_size(height = 500) %>%
+  hc_yAxis(reversedStacks = FALSE)
 
 journal_percent <- journal_data_2 %>%
   hchart("bar", hcaes(x = zeitschrift, y = percent, group = oa_status)) %>%
@@ -147,7 +200,7 @@ journal_percent <- journal_data_2 %>%
            max = 20,
            scrollbar = list(enabled = TRUE)) %>%
   hc_yAxis(labels = list(format = '{value} %'),
-                         max = 100) %>%
+                         max = 100, reversedStacks = FALSE) %>%
   hc_size(height = 500)
 
 
@@ -215,7 +268,7 @@ publications_cha_dashboard <- read_csv("publications_cha_dashboard.csv")
 
 
 bih_data <- publications_cha_dashboard %>%
-  filter(year >= 2018)
+  filter(year >= 2018 & year <= 2020)
 
 #doi_na schauen
 #dois bih anzeigen
---
+++ roadoi.R
@@ -0,0 +1,27 @@
+########################### Load source ###########################
+
+source("data_1.R", encoding = 'UTF-8')
+
+library(roadoi)
+
+data_doi <- data %>%
+  filter(!str_detect(doi, "keine DOI"))
+
+sample_data <- data_doi %>%
+  sample_n(size = 100)
+
+
+########################### Load data from unpaywall ###########################
+
+# https://cran.r-project.org/web/packages/roadoi/vignettes/intro.html
+
+data_unpaywall <- roadoi::oadoi_fetch(dois = data$doi,
+            email = "jan.taubitz@charite.de",
+            .progress = "text")
+
+########################### Proxy settings ###########################
+# https://support.rstudio.com/hc/en-us/articles/200488488-Configuring-R-to-Use-an-HTTP-or-HTTPS-Proxy
+# file.edit('~/.Renviron')
+# http_proxy=http://proxy.charite.de:8080/
+# https_proxy=http://proxy.charite.de:8080/
+# 2x http://...
