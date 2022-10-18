454ba6641914926cd1caad9da9c42f1bd9c71cfa
elegandia <51097851+elegandia@users.noreply.github.com>
2022-10-18 14:34:23 +0200

    annotations

--- data_1.R
+++ data_1.R
@@ -51,7 +51,7 @@ data_clean <- raw_data %>%
   mutate(oa_status = replace_na(oa_status, "no result")) %>%
   mutate(oa_status = factor(oa_status, levels = oa_status_colors)) %>%
   mutate(is_oa = if_else(oa_status %in% c("gold", "hybrid", "green"), TRUE, FALSE), .after = "oa_status") %>%
-  mutate(corresponding_author_cha = if_else(row_number() %in% c(1:6292), TRUE, FALSE), .after = "is_oa") %>%
+  mutate(corresponding_author_cha = if_else(row_number() %in% c(1:6292), TRUE, FALSE), .after = "is_oa") %>%   # FIXME (row numbers)
   select(!c(datenbank, autor_en))
 
 
